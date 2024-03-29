ae920e67f70c292a120bc2dd56448a72e890da2f
tallt2m3 <65245443+Ummok998@users.noreply.github.com>
2024-03-29 12:01:13 -0400

    Update main.py
    fixed a simple issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def midpoint(x1,y1,x2,y2):
-    return (x1+x2)/2, (y1+y2)/y2
+    return (x1+x2)/2, (y1+y2)/2
     
 def distance(x1, y1, x2, y2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
